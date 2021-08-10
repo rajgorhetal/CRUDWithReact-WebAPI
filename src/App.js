7f8c34323b3ce51c64b16bcdcf1fdaf839164c96
hetalrajgor <hetalrajgor26@gmail.com>
2021-08-10 13:08:51 +0530

    Adding Reactjs files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Addstudent from './Addstudent';
+import Studentlist from './Studentlist';
+import EditStudent from './Editstudent';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import '../src/App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navheader">
+          <div className="collapse navbar-collapse" >
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={'/Addstudent'} className="nav-link">Addstudent</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={'/Studentlist'} className="nav-link">Student List</Link>
+              </li>
+            </ul>
+          </div>
+        </nav> <br />
+        <Switch>
+          <Route exact path='/Addstudent' component={Addstudent} />
+          <Route path='/edit/:id' component={EditStudent} />
+          <Route path='/Studentlist' component={Studentlist} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
